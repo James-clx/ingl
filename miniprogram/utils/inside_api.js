99d1561f9ac446717edb3e4bfd3f42962d241d15
james-clx <jamesclx98@gmail.com>
2021-09-11 11:31:17 +0800

    fix(talk)修复说说封禁发表权限无效bug

--- miniprogram/utils/inside_api.js
+++ miniprogram/utils/inside_api.js
@@ -75,7 +75,7 @@ function getBlock(openid) {
       url: 'https://www.inguangli.cn/ingl/api/access/issue_forum',
       method: 'POST',
       data:{
-        "openid":'o1Q1N451krUKupjH1EqboJnBD5UI'
+        "openid":openid
       },
       success (res) {
         userblock = res.data.data
